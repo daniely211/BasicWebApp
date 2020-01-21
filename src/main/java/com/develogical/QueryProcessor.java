11448c8c7bb8bbee0b6e891d6ac0d709ea7d40a1
Daniel <danielyung211@gmail.com>
2020-01-21 11:52:36 +0000

    Added calculating plus and multiplied by

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -10,6 +10,22 @@ public class QueryProcessor {
         }
         String[] splitString = query.split(": ");
         if (splitString.length > 1) {
+            String first = splitString[1];
+            if (first.startsWith("what is")) {
+                String[] split2 = first.split(" ");
+                String num1 = split2[2];
+                String op = split2[3];
+                switch (op) {
+                    case "multiplied":
+                        String num2 = split2[5];
+                        return String.valueOf(Integer.parseInt(num1) * Integer.parseInt(num2));
+                    case "plus":
+                        String num3 = split2[4];
+                        return String.valueOf(Integer.parseInt(num1) + Integer.parseInt(num3));
+                }
+
+            }
+
             switch(splitString[1]) {
                 case "what is your team name":
                     return "Team Golf";
@@ -26,7 +42,8 @@ public class QueryProcessor {
                 default:
                     return "Team Golf";
             }
-
+//            what%20is%204%20multiplied%20by%2014
+//            %20what%20is%205%20plus%2013
 //            %20which%20of%20the%20following%20numbers%20is%20the%20largest:%2042,%2067,%2030,%20614
         }
 
