e20e5dd43165d7e6a8084a770d0371736b9d0905
Daniel <danielyung211@gmail.com>
2020-01-21 11:33:47 +0000

    added number of strings answer

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -8,14 +8,26 @@ public class QueryProcessor {
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         }
-        String[] splitString = query.split(":");
+        String[] splitString = query.split(": ");
         if (splitString.length > 1) {
             switch(splitString[1]) {
-                case " what is your team name":
+                case "what is your team name":
                     return "Team Golf";
+                case "which of the following numbers is the largest":
+                    if (splitString.length > 2) {
+                        String numbers = splitString[2];
+                        String[] numStrings = numbers.split(",");
+                        int max = 0
+                        for(int i = 0; i < numStrings.length; i++) {
+                            max = Math.max(max, Integer.parseInt(numStrings[i]));
+                        }
+                        return String.valueOf(max);
+                    }
                 default:
                     return "Team Golf";
             }
+
+//            %20which%20of%20the%20following%20numbers%20is%20the%20largest:%2042,%2067,%2030,%20614
         }
 
         return "";
