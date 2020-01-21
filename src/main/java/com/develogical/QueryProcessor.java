95b4321a3f4340b5bb09172ceca9d3e5d56d77c0
Daniel <danielyung211@gmail.com>
2020-01-21 12:05:16 +0000

    cube and sqre root

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -39,9 +39,33 @@ public class QueryProcessor {
                         }
                         return String.valueOf(max);
                     }
+                case "which of the following numbers is both a square and a cube":
+                    if (splitString.length > 2) {
+                        String numbers = splitString[2];
+                        String[] numStrings = numbers.split(", ");
+                        StringBuilder stringBuilder = new StringBuilder();
+                        stringBuilder.append("[");
+                        for(int i = 0; i < numStrings.length; i++) {
+                            int cur = Integer.parseInt(numStrings[i]);
+                            double sqreRoot = Math.sqrt(cur); // get the cube root
+                            if (Math.round(sqreRoot) == sqreRoot) {
+                                double cubeRoot = Math.cbrt(cur); // get the cube root
+                                if (Math.round(cubeRoot) == cubeRoot) {
+                                    stringBuilder.append(numStrings[i]);
+                                    stringBuilder.append(", ");
+                                }
+                            }
+                        }
+                        return stringBuilder.toString() + "]";
+                    }
+
                 default:
                     return "Team Golf";
             }
+//            which%20year%20was%20Theresa%20May%20first%20elected%20as%20the%20Prime%20Minister%20of%20Great%20Britain
+//            %20which%20of%20the%20following%20numbers%20are%20primes:%20692,%20449
+//            %20who%20played%20James%20Bond%20in%20the%20film%20Dr%20No
+//            %20which%20of%20the%20following%20numbers%20is%20both%20a%20square%20and%20a%20cube:%20841,%20912
 //            what%20is%204%20multiplied%20by%2014
 //            %20what%20is%205%20plus%2013
 //            %20which%20of%20the%20following%20numbers%20is%20the%20largest:%2042,%2067,%2030,%20614
